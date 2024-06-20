2a3727c7809814feae449fc37517791a3c3e86c1
AD <adhish.g704@gmail.com>
2024-06-20 15:42:47 +0530

    completed navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
-        <span className='red-500'>Anime</span>
+    <div className='w-100 p-4 bg-slate-800 '>
+        <span className='text-3xl ml-5 text-yellow-100 font-semibold font-mono'>Anime-</span>
+        <span className='text-3xl font-semibold font-mono text-gray-300'>Predictor</span>
+        <Link to = "/">
+          <span className='font-mono text-gray-300 ml-44'>Home</span>
+        </Link>
+        <Link to = "/">
+          <span className='font-mono text-gray-300 ml-10'>About</span>
+        </Link>
+        <Link to = "/">
+          <span className='font-mono text-gray-300 ml-10'>Login</span>
+        </Link>
+        <Link to = "/">
+          <span className='font-mono text-white ml-10 p-2 border border-white'>Sign up</span>
+        </Link>
     </div>
   )
 }
