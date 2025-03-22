c660177b8fc8c5baf6118356c2dcec540b3e9051
Adhish G <120158636+Adhishg704@users.noreply.github.com>
2025-03-22 17:36:36 +0530

    Removed unnecessary things to keep the prototype as simple as possible.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
 import Recommend from "./pages/Recommend";
 
 export default function App() {
@@ -11,9 +9,7 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path = "/" element = {<HomePage />} />
-        <Route path = "/signup" element = {<SignUp />} />
-        <Route path = "/login" element = {<Login />} />
+        <Route path = "/" element = {<Recommend />} />
         <Route path = "/recommend" element = {<Recommend />} />
       </Routes>
     </Router>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,18 +6,6 @@ export default function Navbar() {
     <div className='w-100 p-4 bg-slate-800 '>
         <span className='text-3xl ml-5 text-yellow-100 font-semibold font-mono'>Anime-</span>
         <span className='text-3xl font-semibold font-mono text-gray-300'>Analyzer</span>
-        <Link to = "/">
-          <span className='font-mono text-gray-300 ml-44'>Home</span>
-        </Link>
-        <Link to = "/recommend">
-          <span className='font-mono text-gray-300 ml-10'>Recommend</span>
-        </Link>
-        <Link to = "/login">
-          <span className='font-mono text-gray-300 ml-10'>Login</span>
-        </Link>
-        <Link to = "/signup">
-          <span className='font-mono text-white ml-10 p-2 border border-white'>Sign up</span>
-        </Link>
     </div>
   )
 }
